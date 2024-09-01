6fc4c32289fdb528e86289c43f5386fa24327fc0
PhuongVu <puong334@gmail.com>
2024-09-01 22:38:09 +0700

    Skill&About

--- src/components/Content/Skills.tsx
+++ src/components/Content/Skills.tsx
@@ -1,46 +1,79 @@
 
 
+import ProgressBar from "@ramonak/react-progress-bar";
 
 const SkillPortfolio = () => {
 
     return (
         <>
-            <div className="arlo_tm_section">
+            <div className="arlo_tm_section" id='skills'>
                 <div className="arlo_tm_skills_wrap">
                     <div className="container">
                         <div className="inner_wrap">
                             <div className="leftbox">
                                 <div className="arlo_tm_mini_title_holder">
-                                    <h4>Some About my Abilities</h4>
+                                    <h4>KỸ NĂNG FRONT-END</h4>
                                 </div>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since. Lorem Ipsum has been the industry. Lorem Ipsum has been the industry's standard dummy text since. Lorem Ipsum is simply.</p>
+                                <li>Cắt ghép template Website Responsive</li>
+                                <li>Framework/Library: Bootstrap, Antd, React.js, JQuery</li>
+                                {/* <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since. Lorem Ipsum has been the industry. Lorem Ipsum has been the industry's standard dummy text since. Lorem Ipsum is simply.</p> */}
                             </div>
                             <div className="rightbox">
                                 <div className="progress_bar_wrap_total">
                                     <div className="arlo_tm_progress_wrap" data-size="small" data-round="c" data-strip="off">
                                         <div className="arlo_tm_progress" data-value="95" data-color="#000">
-                                            <span><span className="label">Wordpress - <span className="experience">5 years of experience</span></span><span className="number">95%</span></span>
-                                            <div className="arlo_tm_bar_bg"><div className="arlo_tm_bar_wrap"><div className="arlo_tm_bar"></div></div></div>
+                                            <span><span className="label">HTML/CSS/JAVASCRIPT</span><span className="number">95%</span></span>
+                                            {/* <div className="arlo_tm_bar_bg"><div className="arlo_tm_bar_wrap"><div className="arlo_tm_bar"></div></div></div> */}
+                                            <ProgressBar
+                                                completed={95}
+                                                bgColor="#7b8381"
+                                                height="10px"
+                                                baseBgColor="#d80e5a"
+                                                labelColor="white"
+                                                labelSize="10px"
+                                                animateOnRender
+                                                isLabelVisible={false}
+                                            />
                                         </div>
-                                        <div className="arlo_tm_progress" data-value="85" data-color="#000">
-                                            <span><span className="label">Css - <span className="experience">3 years of experience</span></span><span className="number">85%</span></span>
-                                            <div className="arlo_tm_bar_bg"><div className="arlo_tm_bar_wrap"><div className="arlo_tm_bar"></div></div></div>
+                                        <div className="arlo_tm_progress" data-value="95" data-color="#000">
+                                            <span><span className="label">REACT.JS (TYPESCRIPT)</span><span className="number">95%</span></span>
+                                            {/* <div className="arlo_tm_bar_bg"><div className="arlo_tm_bar_wrap"><div className="arlo_tm_bar"></div></div></div> */}
+                                            <ProgressBar
+                                                completed={95}
+                                                bgColor="#7b8381"
+                                                height="10px"
+                                                baseBgColor="#d80e5a"
+                                                labelColor="white"
+                                                labelSize="10px"
+                                                animateOnRender
+                                                isLabelVisible={false}
+                                            />
                                         </div>
-                                        <div className="arlo_tm_progress" data-value="75" data-color="#000">
-                                            <span><span className="label">HTML - <span className="experience">4 years of experience</span></span><span className="number">75%</span></span>
-                                            <div className="arlo_tm_bar_bg"><div className="arlo_tm_bar_wrap"><div className="arlo_tm_bar"></div></div></div>
+                                        <div className="arlo_tm_progress" data-value="95" data-color="#000">
+                                            <span><span className="label">DESIGN UI/UX</span><span className="number">95%</span></span>
+                                            {/* <div className="arlo_tm_bar_bg"><div className="arlo_tm_bar_wrap"><div className="arlo_tm_bar"></div></div></div> */}
+                                            <ProgressBar
+                                                completed={95}
+                                                bgColor="#7b8381"
+                                                height="10px"
+                                                baseBgColor="#d80e5a"
+                                                labelColor="white"
+                                                labelSize="10px"
+                                                animateOnRender
+                                                isLabelVisible={false}
+                                            />
                                         </div>
-                                        <div className="arlo_tm_progress" data-value="90" data-color="#000">
+                                        {/* <div className="arlo_tm_progress" data-value="90" data-color="#000">
                                             <span><span className="label">After Effect - <span className="experience">6 years of experience</span></span><span className="number">90%</span></span>
                                             <div className="arlo_tm_bar_bg"><div className="arlo_tm_bar_wrap"><div className="arlo_tm_bar"></div></div></div>
-                                        </div>
+                                        </div> */}
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div className="container">
+                    {/* <div className="container">
                         <div className="inner_wrap">
                             <div className="leftbox">
                                 <div className="arlo_tm_mini_title_holder">
@@ -71,7 +104,7 @@ const SkillPortfolio = () => {
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </>
--- src/components/leftPart.tsx
+++ src/components/leftPart.tsx
@@ -15,6 +15,7 @@ const LeftPart = () => {
                         <ul className="anchor_nav">
                             <li><a href="#home">Home</a></li>
                             <li><a href="#about">About</a></li>
+                            <li><a href="#skills">Skills</a></li>
                             <li><a href="#services">Project</a></li>
                             <li><a href="#portfolio">Portfolio</a></li>
 
